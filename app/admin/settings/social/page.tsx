3a4662f3a2c42cfe0f2b23ab5dc52dd6a344e36a
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2026-02-04 12:44:21 +0600

    add/social form settings

---
+++ app/admin/settings/social/page.tsx
@@ -0,0 +1,13 @@
+import { Title } from "@mantine/core";
+import SocialForm from "./SocialForm";
+
+function SocialPage() {
+  return (
+    <div>
+      <Title size="lg">Social Settings</Title>
+      <SocialForm />
+    </div>
+  );
+}
+
+export default SocialPage;
